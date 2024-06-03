dc0d090b7469a6d61729a2d9f95b2f46d795e54c
Shiham123 <s36651786@gmail.com>
2024-06-03 21:59:46 +0600

    left section complete without responsive now time to focus responsive

--- src/Shared/Button.jsx
+++ src/Shared/Button.jsx
@@ -1,11 +1,20 @@
 import PropTypes from "prop-types"
 import {CiMail} from "react-icons/ci"
+import {useDispatch, useSelector} from "react-redux"
+import {hoverChange} from "../app/theme"
 
 const Button = ({text}) => {
+	const {isHover} = useSelector((state) => state.theme)
+	const dispatch = useDispatch()
+
 	return (
-		<button className="border-[1px] border-[#27e68a] my-4 py-4 mx-6 rounded-full bg-[#27e68a] hover:bg-primary hover:text-[#27e68a] flex justify-center items-center gap-4 hover:transition-all hover:duration-300 hover:ease-in-out">
-			<CiMail size={30} color="#1f1f1f" />
-			<span className="font-Inter font-medium text-xl uppercase leading-3 tracking-wide">
+		<button
+			onMouseEnter={() => dispatch(hoverChange(true))}
+			onMouseLeave={() => dispatch(hoverChange(false))}
+			className="border-[1px] border-[#27e68a] my-4 py-4 mx-6 rounded-full bg-[#27e68a] group hover:bg-primary flex justify-center items-center gap-4 hover:transition-all hover:duration-300 hover:ease-in-out"
+		>
+			<CiMail size={30} color={`${isHover ? " #27e68a" : "#1f1f1f"}`} />
+			<span className="font-Inter font-medium text-xl uppercase leading-3 tracking-wide group-hover:text-[#27e68a]">
 				{text}
 			</span>
 		</button>
