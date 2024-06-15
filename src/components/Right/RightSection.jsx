469e30f105edcbbff278a9cff7f249ed3ffa12be
Shiham123 <s36651786@gmail.com>
2024-06-15 16:15:29 +0600

    all icons installed for sidebar

--- src/components/Right/RightSection.jsx
+++ src/components/Right/RightSection.jsx
@@ -1,7 +1,29 @@
-import { GoHome } from "react-icons/go";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {TextMessage} from "@icon-park/react"
+
+import {
+	faUserTie,
+	faBriefcase,
+	faBarsStaggered,
+	faCode,
+	faTarp,
+} from "@fortawesome/free-solid-svg-icons"
+import {GoHome} from "react-icons/go"
+import {EnvelopeIcon} from "@heroicons/react/24/outline"
 
 const RightSection = () => {
-	return <div className="col-span-2 xl:col-span-1"></div>
+	return (
+		<div className="col-span-2 xl:col-span-1">
+			<GoHome />
+			<FontAwesomeIcon icon={faUserTie} />
+			<FontAwesomeIcon icon={faBriefcase} />
+			<FontAwesomeIcon icon={faBarsStaggered} />
+			<FontAwesomeIcon icon={faCode} />
+			<FontAwesomeIcon icon={faTarp} />
+			<TextMessage theme="outline" size="24" fill="#ffffff" />
+			<EnvelopeIcon className="w-6 h-6" />
+		</div>
+	)
 }
 
 export default RightSection
