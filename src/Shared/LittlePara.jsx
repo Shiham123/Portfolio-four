09e5ac1fa15e0b51386a11ed88deb54d0e07a1e5
Shiham123 <s36651786@gmail.com>
2024-06-15 12:04:43 +0600

    little para

--- src/Shared/LittlePara.jsx
+++ src/Shared/LittlePara.jsx
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types"
 
 const LittlePara = ({paraText}) => {
-	return <p className="text-paraColor my-6">{paraText}</p>
+	return (
+		<p className="text-paraColor py-4 leading-[30px] md:px-6 sm:px-6 lg:px-6 xl:px-6 xxs:px-6 font-Inter text-[16px]">
+			{paraText}
+		</p>
+	)
 }
 
 export default LittlePara
