10bd1be24356cf5cba39408cd1672ac3e51ab0d8
Shiham123 <s36651786@gmail.com>
2024-06-16 20:37:48 +0600

    running side bar desing

--- src/components/Left/Sidebar.jsx
+++ src/components/Left/Sidebar.jsx
@@ -1,28 +1,44 @@
 import {FaDiscord, FaGithub, FaLinkedin, FaWhatsapp} from "react-icons/fa"
+import PropTypes from "prop-types"
 
+// SidebarItem Component
+const SidebarItem = ({text}) => {
+	return <li>{text}</li>
+}
+
+// Sidebar Component
 const Sidebar = () => {
 	return (
-		<div className="lg:top-0 lg:right-0 bg-bgSidebar h-full fixed">
-			<ul>
-				Menu
-				<li>About</li>
-				<li>Resume</li>
-				<li>Services</li>
-				<li>Skills</li>
-				<li>Portfolio</li>
-				<li>Testimonial</li>
-				<li>Contact</li>
-			</ul>
+		<>
+			{/* Backdrop for blur effect */}
+			<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-10 bg-[#ffffff] z-10"></div>
 
-			<div>
-				<p>Social</p>
-				<FaLinkedin />
-				<FaGithub />
-				<FaWhatsapp />
-				<FaDiscord />
+			{/* Sidebar */}
+			<div className="fixed top-0 right-0 h-full bg-bgSidebar flex flex-col justify-center items-center lg:w-[30%] z-20">
+				<ul className="text-left">
+					<SidebarItem text="About" />
+					<SidebarItem text="Resume" />
+					<SidebarItem text="Services" />
+					<SidebarItem text="Skills" />
+					<SidebarItem text="Portfolio" />
+					<SidebarItem text="Testimonial" />
+					<SidebarItem text="Contact" />
+				</ul>
+
+				<div className="text-left">
+					<p>Social</p>
+					<div className="flex justify-center">
+						<FaLinkedin />
+						<FaGithub />
+						<FaWhatsapp />
+						<FaDiscord />
+					</div>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
 export default Sidebar
+
+SidebarItem.propTypes = {text: PropTypes.string.isRequired}
