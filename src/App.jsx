b8ea2db6d95608234a384fb9559404ee8e34bae4
Shiham123 <s36651786@gmail.com>
2024-06-21 12:36:38 +0600

    sm devices responsive running

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import RightSection from "./components/Right/RightSection"
 const App = () => {
 	const {isSidebarOpen} = useSelector((state) => state.theme)
 	return (
-		<div className="grid grid-cols-12 2xl:grid 2xl:grid-cols-12 xl:grid xl:grid-cols-12 lg:flex lg:flex-col lg:justify-center lg:items-center lg:container lg:mx-auto lg:py-[5rem] lg:relative md:flex md:flex-col md:justify-center md:items-center md:container md:mx-auto md:relative md:py-[3rem]">
+		<div className="grid grid-cols-12 2xl:grid 2xl:grid-cols-12 xl:grid xl:grid-cols-12 lg:flex lg:flex-col lg:justify-center lg:items-center lg:container lg:mx-auto lg:py-[5rem] lg:relative md:flex md:flex-col md:justify-center md:items-center md:container md:mx-auto md:relative md:py-[3rem] sm:flex sm:flex-col sm:justify-center sm:items-center sm:container sm:mx-auto sm:relative sm:py-[3rem]">
 			<LeftSection />
 			<MiddleSection />
 			<RightSection />
