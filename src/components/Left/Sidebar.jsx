f8e3dccc715f483c8d0b129cf6ba105ee0eb34cf
Shiham123 <s36651786@gmail.com>
2024-06-21 08:36:49 +0600

    side bar hover color applied

--- src/components/Left/Sidebar.jsx
+++ src/components/Left/Sidebar.jsx
@@ -23,44 +23,84 @@ const Sidebar = () => {
 				<ul className="flex flex-col justify-start items-start gap-6">
 					<li className="font-Inter text-sidebarText capitalize lg:text-2xl tracking-wide">Menu</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaHome color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">About</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaHome
+							size={20}
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							About
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaUserTie color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Resume</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaUserTie
+							size={20}
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Resume
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaBriefcase color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Resume</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaBriefcase
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+							size={20}
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Resume
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaBarsStaggered color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Services</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaBarsStaggered
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+							size={20}
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Services
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaCode color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Skills</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaCode
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+							size={20}
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Skills
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaTarp color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Portfolio</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaTarp
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+							size={20}
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Portfolio
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaRegMessage color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Testimonial</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaRegMessage
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+							size={20}
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Testimonial
+						</span>
 					</li>
 
-					<li className="flex justify-start items-center gap-4">
-						<FaEnvelope color="#8f8f8f" size={20} />
-						<span className="font-Inter text-sidebarText text-lg">Contact</span>
+					<li className="flex justify-start items-center gap-4 group">
+						<FaEnvelope
+							className="transition-all ease-in-out duration-200 group-hover:text-secondary text-sidebarText"
+							size={20}
+						/>
+						<span className="font-Inter text-sidebarText group-hover:text-secondary text-lg transition-all duration-200 cursor-pointer">
+							Contact
+						</span>
 					</li>
 
 					<p className="font-Inter text-sidebarText font-bold tracking-widest text-lg pt-6">
@@ -72,20 +112,32 @@ const Sidebar = () => {
 							target="_blank"
 							rel="noopener noreferrer"
 						>
-							<FaLinkedin size={25} color="#8f8f8f" />
+							<FaLinkedin
+								size={25}
+								className="transition-all ease-in-out duration-200 hover:text-secondary text-sidebarText"
+							/>
 						</a>
 						<a href="https://github.com/Shiham123" target="_blank" rel="noopener noreferrer">
-							<FaGithub size={25} color="#8f8f8f" />
+							<FaGithub
+								size={25}
+								className="transition-all ease-in-out duration-200 hover:text-secondary text-sidebarText"
+							/>
 						</a>
 						<a href="https://wa.me/+8801327239397" target="_blank" rel="noopener noreferrer">
-							<FaWhatsapp size={25} color="#8f8f8f" />
+							<FaWhatsapp
+								size={25}
+								className="transition-all ease-in-out duration-200 hover:text-secondary text-sidebarText"
+							/>
 						</a>
 						<a
 							href="https://discordapp.com/users/1022503770321584178"
 							target="_blank"
 							rel="noopener noreferrer"
 						>
-							<FaDiscord size={25} color="#8f8f8f" />
+							<FaDiscord
+								size={25}
+								className="transition-all ease-in-out duration-200 hover:text-secondary text-sidebarText"
+							/>
 						</a>
 					</div>
 				</ul>
