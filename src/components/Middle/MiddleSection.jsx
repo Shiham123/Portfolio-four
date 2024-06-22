fcf7d6f2144bfad3170730785ad5430d3434c226
Shiham123 <s36651786@gmail.com>
2024-06-22 12:04:12 +0600

    going for stepper

--- src/components/Middle/MiddleSection.jsx
+++ src/components/Middle/MiddleSection.jsx
@@ -1,4 +1,5 @@
 import About from "./About"
+import Experience from "./Experience"
 import Introduction from "./Introduction"
 
 const MiddleSection = () => {
@@ -6,6 +7,7 @@ const MiddleSection = () => {
 		<section className="2xl:col-span-8 2xl:col-start-4 2xl:col-end-12 2xl:mt-[6%] xl:col-span-8 xl:col-start-4 xl:col-end-12 xl:mt-[8%]">
 			<Introduction />
 			<About />
+			<Experience />
 		</section>
 	)
 }
---
+++ src/components/Middle/Stepper.jsx
@@ -0,0 +1,5 @@
+const Stepper = () => {
+	return <div></div>
+}
+
+export default Stepper
