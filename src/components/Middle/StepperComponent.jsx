7b98d9336350c1edd294235786ebe80741a923df
Shiham123 <s36651786@gmail.com>
2024-06-23 22:29:24 +0600

    stepper running

--- src/components/Middle/StepperComponent.jsx
+++ src/components/Middle/StepperComponent.jsx
@@ -1,5 +1,20 @@
+import styles from "../../scss/stepper.module.scss"
+
 const StepperComponent = () => {
-	return <section className="w-[800px]"></section>
+	return (
+		<section className="w-[800px]">
+			<div className={styles.timeline}>
+				<div className="relative">
+					<div className={styles.dot}></div>
+					<div className="pl-10">
+						<span className="timeline-date text-secondary">13 November 2021</span>
+						<h3 className="timeline-title text-secondary">Event 1</h3>
+						<p className="text-secondary">This is my education timeline from my history</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default StepperComponent
