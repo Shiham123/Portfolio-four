b6c56a413aadd4867f1dc10c5f508df7f9296720
Shiham123 <s36651786@gmail.com>
2024-06-25 19:52:44 +0600

    education and experience section done

--- src/components/Middle/EducationExperience/StepItem.jsx
+++ src/components/Middle/EducationExperience/StepItem.jsx
@@ -2,16 +2,22 @@ import PropTypes from "prop-types"
 
 const StepItem = ({topText, middleText, lastText}) => {
 	return (
-		<div className="step-item flex flex-col justify-start items-start relative py-8 pl-12">
-			<h6 className="text-paraColor font-Inter text-lg capitalize tracking-wide hover:text-secondary transition-all duration-200 ease-in-out">
-				{topText}
-			</h6>
-			<div className="step-dot absolute w-4 h-4 rounded-full top-1/4 -left-2 bg-secondary"></div>
-			<h2 className="text-primaryFont text-2xl font-Inter font-light tracking-wide opacity-90">
-				{middleText}
-			</h2>
-			<h6 className="text-paraColor tracking-wider font-Inter text-sm">{lastText}</h6>
-		</div>
+		<>
+			<div className="step-item flex flex-col justify-start items-start relative py-8 pl-12">
+				{/* and this is absolute section */}
+				<div className="absolute top-[25%] left-0 h-[100%] w-[1px] bg-secondary"></div>
+				<div className="step-dot absolute w-4 h-4 rounded-[100%] top-[25%] -left-2 bg-secondary"></div>
+
+				{/* content */}
+				<h6 className="text-paraColor font-Inter text-lg capitalize tracking-wide hover:text-secondary transition-all duration-200 ease-in-out">
+					{topText}
+				</h6>
+				<h2 className="text-primaryFont text-2xl font-Inter font-light tracking-wide opacity-90">
+					{middleText}
+				</h2>
+				<h6 className="text-paraColor tracking-wider font-Inter text-sm">{lastText}</h6>
+			</div>
+		</>
 	)
 }
 
--- src/components/Middle/EducationExperience/StepperComponent.jsx
+++ src/components/Middle/EducationExperience/StepperComponent.jsx
@@ -3,7 +3,6 @@ import StepItem from "./StepItem"
 const StepperComponent = () => {
 	return (
 		<section className="relative pl-8">
-			<div className="absolute left-4 top-0 bottom-0 w-px bg-secondary"></div>
 			<StepItem
 				topText="2024 - present"
 				middleText="Frontend Developer"
