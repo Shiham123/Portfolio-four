12fc3eb5b49478d2fb57838aa012d3f870c20526
Shiham <s36651786@gmail.com>
2024-06-29 12:33:07 +0600

    tailwind setup and my services page complete

--- src/components/Middle/ServicesSection/ServiceCard.jsx
+++ src/components/Middle/ServicesSection/ServiceCard.jsx
@@ -1,22 +1,31 @@
 import PropTypes from "prop-types"
 
-const ServiceCard = ({headingText, paraText, hyperText}) => {
+const ServiceCard = ({headingText, paraText, hyperText, Icon}) => {
 	return (
-		<div className="border-[1px] border-primaryBorder w-10/12 rounded-custom p-8 hover:border-secondary transition-all duration-300 ease-in">
-			<h3 className="font-Inter text-primaryFont font-light tracking-wide capitalize text-2xl leading-10">
-				{headingText}
-			</h3>
-			<p className="font-Inter text-paraColor font-light tracking-wide first-letter:capitalize leading-10">
-				{paraText}
-			</p>
-			<a
-				className="font-Inter text-primaryFont font-light tracking-wide first-letter:capitalize underline leading-10"
-				href="https://github.com/Shiham123"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				{hyperText}
-			</a>
+		<div className="grid grid-cols-8 border-[1px] border-primaryBorder w-10/12 h-fit rounded-custom p-8 hover:border-secondary transition-all duration-300 ease-in group">
+			{/* div one */}
+			<div className="col-span-6">
+				<h3 className="font-Inter text-primaryFont font-light tracking-wide capitalize text-2xl leading-10">
+					{headingText}
+				</h3>
+				<p className="font-Inter text-paraColor font-light tracking-wide first-letter:capitalize leading-10 pb-6">
+					{paraText}
+				</p>
+				<a
+					className="font-Inter text-primaryFont font-light tracking-wide first-letter:capitalize group-hover:underline"
+					href="https://github.com/Shiham123"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{hyperText}
+				</a>
+			</div>
+			{/* icon div */}
+			<div className="col-span-2">
+				<div className="grid justify-items-center">
+					<Icon className="text-secondary" size={30} />
+				</div>
+			</div>
 		</div>
 	)
 }
@@ -27,4 +36,5 @@ ServiceCard.propTypes = {
 	headingText: PropTypes.string.isRequired,
 	paraText: PropTypes.string.isRequired,
 	hyperText: PropTypes.string.isRequired,
+	Icon: PropTypes.element.isRequired,
 }
--- src/components/Middle/ServicesSection/ServicesSection.jsx
+++ src/components/Middle/ServicesSection/ServicesSection.jsx
@@ -1,38 +1,49 @@
-import {FaBarsStaggered} from "react-icons/fa6"
 import LittleHeading from "../../../Shared/LittleHeading"
 import ServiceCard from "./ServiceCard"
 import SubHeading from "../../../Shared/SubHeading"
+
+// icons
+import {FaBarsStaggered} from "react-icons/fa6"
+import {HiOutlineCubeTransparent, HiOutlineServer} from "react-icons/hi2"
+import {PiPaletteLight} from "react-icons/pi"
+import {CiLock} from "react-icons/ci"
+import {LiaTasksSolid} from "react-icons/lia"
 
 const ServicesSection = () => {
 	return (
 		<section className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem]">
 			<LittleHeading headingText="services" Icon={FaBarsStaggered} />
 			<SubHeading mainText="my" spanText="Specialization" />
-			<section className="flex justify-center items-start flex-col gap-8">
+			<section className="flex justify-center items-start flex-col gap-8 my-12">
 				<ServiceCard
 					headingText="MERN Stack Development"
 					paraText="I develop full-stack applications using MongoDB, ExpressJS, ReactJS, and NodeJS."
 					hyperText="Various Projects"
+					Icon={HiOutlineCubeTransparent}
 				/>
 				<ServiceCard
 					headingText="Frontend Development"
 					paraText="I create engaging and responsive web designs using technologies like ReactJS, TailwindCSS, and SCSS."
 					hyperText="Multiple Projects"
+					Icon={PiPaletteLight}
 				/>
 				<ServiceCard
 					headingText="Backend Development"
 					paraText="I build robust backends with NodeJS, ExpressJS, and Mongoose for managing MongoDB."
 					hyperText="Several Projects"
+					Icon={HiOutlineServer}
 				/>
 				<ServiceCard
 					headingText="Authentication Systems"
 					paraText="I implement secure authentication systems using Firebase, JWT, and OAuth."
 					hyperText="Several Implementations"
+					Icon={CiLock}
 				/>
 				<ServiceCard
 					headingText="State Management"
 					paraText="I manage application state efficiently using Redux and Redux-toolkit."
 					hyperText="Multiple Projects"
+					Icon={LiaTasksSolid}
 				/>
 			</section>
 		</section>
