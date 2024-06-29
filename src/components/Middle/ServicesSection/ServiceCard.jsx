60fb043f29e76197b27c39621be338850fa2a3a2
Shiham <s36651786@gmail.com>
2024-06-29 11:47:17 +0600

    services section design

--- src/components/Middle/ServicesSection/ServiceCard.jsx
+++ src/components/Middle/ServicesSection/ServiceCard.jsx
@@ -2,10 +2,18 @@ import PropTypes from "prop-types"
 
 const ServiceCard = ({headingText, paraText, hyperText}) => {
 	return (
-		<div>
-			<h3>{headingText}</h3>
-			<p>{paraText}</p>
-			<a href="https://github.com/Shiham123" target="_blank">
+		<div className="border-[1px] border-primaryBorder w-10/12 rounded-custom p-8 hover:border-secondary transition-all duration-300 ease-in">
+			<h3 className="font-Inter text-primaryFont font-light tracking-wide capitalize text-2xl leading-10">
+				{headingText}
+			</h3>
+			<p className="font-Inter text-paraColor font-light tracking-wide first-letter:capitalize leading-10">
+				{paraText}
+			</p>
+			<a
+				className="font-Inter text-primaryFont font-light tracking-wide first-letter:capitalize underline leading-10"
+				href="https://github.com/Shiham123"
+				target="_blank"
+			>
 				{hyperText}
 			</a>
 		</div>
--- src/components/Middle/ServicesSection/ServicesSection.jsx
+++ src/components/Middle/ServicesSection/ServicesSection.jsx
@@ -9,11 +9,23 @@ const ServicesSection = () => {
 		<section className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem]">
 			<LittleHeading headingText="services" Icon={FaBarsStaggered} />
 			<SubHeading mainText="my" spanText="Specialization" />
-			<ServiceCard
-				headingText="Website Design"
-				paraText="I created digital products with unique ideas use Figma & Framer"
-				hyperText="24 Projects"
-			/>
+			<section className="flex justify-center items-start flex-col gap-8">
+				<ServiceCard
+					headingText="Website Design"
+					paraText="I created digital products with unique ideas use Figma & Framer"
+					hyperText="24 Projects"
+				/>
+				<ServiceCard
+					headingText="Website Design"
+					paraText="I created digital products with unique ideas use Figma & Framer"
+					hyperText="24 Projects"
+				/>
+				<ServiceCard
+					headingText="Website Design"
+					paraText="I created digital products with unique ideas use Figma & Framer"
+					hyperText="24 Projects"
+				/>
+			</section>
 		</section>
 	)
 }
