93322953b0adf47e2eddd0222ad9d52cce85f22c
Shiham <s36651786@gmail.com>
2024-09-06 20:22:07 +0600

    js icon added

--- src/components/Middle/Advantage/Advantage.jsx
+++ src/components/Middle/Advantage/Advantage.jsx
@@ -9,13 +9,16 @@ import HtmlSvgFile from "../../../Utilities/HtmlSvgFile"
 import ScssSvgFile from "../../../Utilities/ScssSvgFile"
 import TailwindcssSvgFile from "../../../Utilities/TailwindcssSvgFile"
 import BootstrapSvgFile from "../../../Utilities/BootstrapSvgFile"
+import JsSvgFile from "../../../Utilities/JsSvgFile"
 
-const skillsDataArray = [
+const skillsDataArrayOne = [
 	{number: 100, Icon: HtmlSvgFile, Id: 1, skillName: "html"},
 	{number: 100, Icon: ScssSvgFile, Id: 2, skillName: "scss"},
 	{number: 100, Icon: TailwindcssSvgFile, Id: 3, skillName: "tailwindcss"},
 	{number: 100, Icon: BootstrapSvgFile, Id: 4, skillName: "Bootstrap"},
 ]
+
+const skillsDataArrayTwo = [{number: 100, Icon: JsSvgFile, Id: 5, skillName: "javaScript"}]
 
 const Advantage = () => {
 	return (
@@ -26,11 +29,20 @@ const Advantage = () => {
 
 			{/* card Block */}
 
-			<div className="grid grid-cols-4 justify-items-start mt-10">
-				{skillsDataArray.map((skill) => {
+			<div className="grid grid-cols-4 justify-items-start mt-10 gap-y-[5rem]">
+				{skillsDataArrayOne.map((skill) => {
 					const {number, Icon, Id, skillName} = skill
 					return <SkillsCard key={Id} number={number} Icon={Icon} Id={Id} skillName={skillName} />
 				})}
+				{skillsDataArrayTwo.map((skill) => (
+					<SkillsCard
+						key={skill.Id}
+						number={skill.number}
+						Icon={skill.Icon}
+						Id={skill.Id}
+						skillName={skill.skillName}
+					/>
+				))}
 			</div>
 		</section>
 	)
