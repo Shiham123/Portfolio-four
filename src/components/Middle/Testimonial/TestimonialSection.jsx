838ec466fa8b4790260e49b5a75c3c93dbc21624
Shiham <s36651786@gmail.com>
2024-09-16 21:14:53 +0600

    custom icon setup

--- src/components/Middle/Testimonial/TestimonialSection.jsx
+++ src/components/Middle/Testimonial/TestimonialSection.jsx
@@ -31,15 +31,63 @@ const TestimonialSection = () => {
 	}, [dispatch])
 
 	return (
-		<div className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem] rounded-[20px]">
+		<div className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem] rounded-[20px] relative">
 			<LittleHeading headingText="portfolio" Icon={BiMessageAltDetail} />
 			<SubHeading mainText="Trusted by my" spanText="most valued individuals" />
 
 			{status === "succeeded" ? (
 				<>
 					{/* Custom Previous and Next Buttons */}
-					<div className="swiper-button-prev-custom">Prev</div>
-					<div className="swiper-button-next-custom">Next</div>
+
+					<div className="swiper-button-prev-custom absolute bottom-[-4rem] left-[6rem] border-2 border-primaryBorder p-3 rounded-[100%] cursor-pointer">
+						<svg
+							width="20px"
+							height="20px"
+							viewBox="-4.5 0 20 20"
+							version="1.1"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+								<g
+									id="Dribbble-Light-Preview"
+									transform="translate(-305.000000, -6679.000000)"
+									fill="#565656"
+								>
+									<g id="icons" transform="translate(56.000000, 160.000000)">
+										<path
+											d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769"
+											id="arrow_left-[#336]"
+											transform="scale(-1, 1) translate(-510, 0)"
+										></path>
+									</g>
+								</g>
+							</g>
+						</svg>
+					</div>
+					<div className="swiper-button-next-custom absolute bottom-[-4rem] left-[10rem] border-2 border-primaryBorder p-3 rounded-[100%] cursor-pointer">
+						<svg
+							width="20px"
+							height="20px"
+							viewBox="-4.5 0 20 20"
+							version="1.1"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+								<g
+									id="Dribbble-Light-Preview"
+									transform="translate(-305.000000, -6679.000000)"
+									fill="#565656"
+								>
+									<g id="icons" transform="translate(56.000000, 160.000000)">
+										<path
+											d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769"
+											id="arrow_right-[#336]"
+										></path>
+									</g>
+								</g>
+							</g>
+						</svg>
+					</div>
 
 					<Swiper
 						pagination={{type: "fraction"}}
