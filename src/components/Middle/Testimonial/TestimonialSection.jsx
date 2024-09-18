369996ca4a08a2214bf9bd00e241e9986adbc2c1
Shiham <s36651786@gmail.com>
2024-09-18 21:13:32 +0600

    icon design complete with redux state management

--- src/components/Middle/Testimonial/TestimonialSection.jsx
+++ src/components/Middle/Testimonial/TestimonialSection.jsx
@@ -5,10 +5,13 @@ import SubHeading from "../../../Shared/SubHeading"
 import TestimonialCard from "./TestimonialCard"
 import {fetchTestimonial} from "../../../app/testimonialSlice"
 import {BiMessageAltDetail} from "react-icons/bi"
+import {setFillColorId} from "../../../app/handleSvg"
 
 const TestimonialSection = () => {
 	const dispatch = useDispatch()
 	const {data, status, error} = useSelector((state) => state.testimonial)
+	const {fillColor, id} = useSelector((state) => state.icon)
+
 	const [currentIndex, setCurrentIndex] = useState(0)
 	const [fadeIn, setFadeIn] = useState(true)
 
@@ -29,6 +32,8 @@ const TestimonialSection = () => {
 			return () => clearInterval(interval)
 		}
 	}, [data, status])
+
+	const changeIconColor = () => {}
 
 	return (
 		<div className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem] rounded-[20px] relative">
@@ -59,53 +64,74 @@ const TestimonialSection = () => {
 				<p>{error ? `Error: ${error}` : "Loading testimonials..."}</p>
 			)}
 
-			<div>
-				<svg
-					width="20px"
-					height="20px"
-					viewBox="-4.5 0 20 20"
-					version="1.1"
-					xmlns="http://www.w3.org/2000/svg"
+			<div className="flex justify-start items-start gap-4">
+				<div
+					onMouseOver={() => {
+						dispatch(setFillColorId({color: "#28e98c", id: "svg01"}))
+					}}
+					onMouseLeave={() => {
+						dispatch(setFillColorId({color: "#565656", id: "svg01"}))
+					}}
+					className="border-2 border-primaryBorder px-4 py-4 rounded-[100%] hover:border-secondary transition-all duration-300 cursor-pointer"
 				>
-					<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-						<g
-							id="Dribbble-Light-Preview"
-							transform="translate(-305.000000, -6679.000000)"
-							fill="#565656"
-						>
-							<g id="icons" transform="translate(56.000000, 160.000000)">
-								<path
-									d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769"
-									id="arrow_left-[#336]"
-									transform="scale(-1, 1) translate(-510, 0)"
-								></path>
+					<svg
+						width="20px"
+						height="20px"
+						viewBox="-4.5 0 20 20"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+							<g
+								className="transition-all duration-300"
+								id="Dribbble-Light-Preview"
+								transform="translate(-305.000000, -6679.000000)"
+								fill={`${id === "svg01" ? fillColor : "#565656"}`}
+							>
+								<g id="icons" transform="translate(56.000000, 160.000000)">
+									<path
+										d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769"
+										id="arrow_left-[#336]"
+										transform="scale(-1, 1) translate(-510, 0)"
+									></path>
+								</g>
 							</g>
 						</g>
-					</g>
-				</svg>
+					</svg>
+				</div>
 
-				<svg
-					width="20px"
-					height="20px"
-					viewBox="-4.5 0 20 20"
-					version="1.1"
-					xmlns="http://www.w3.org/2000/svg"
+				<div
+					onMouseOver={() => {
+						dispatch(setFillColorId({color: "#28e98c", id: "svg02"}))
+					}}
+					onMouseLeave={() => {
+						dispatch(setFillColorId({color: "#565656", id: "svg02"}))
+					}}
+					className="border-2 border-primaryBorder px-4 py-4 rounded-[100%] cursor-pointer hover:border-secondary transition-all duration-300"
 				>
-					<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-						<g
-							id="Dribbble-Light-Preview"
-							transform="translate(-305.000000, -6679.000000)"
-							fill="#565656"
-						>
-							<g id="icons" transform="translate(56.000000, 160.000000)">
-								<path
-									d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769"
-									id="arrow_right-[#336]"
-								></path>
+					<svg
+						width="20px"
+						height="20px"
+						viewBox="-4.5 0 20 20"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+							<g
+								id="Dribbble-Light-Preview"
+								transform="translate(-305.000000, -6679.000000)"
+								fill={`${id === "svg02" ? fillColor : "#565656"}`}
+							>
+								<g id="icons" transform="translate(56.000000, 160.000000)">
+									<path
+										d="M249.365851,6538.70769 L249.365851,6538.70769 C249.770764,6539.09744 250.426289,6539.09744 250.830166,6538.70769 L259.393407,6530.44413 C260.202198,6529.66364 260.202198,6528.39747 259.393407,6527.61699 L250.768031,6519.29246 C250.367261,6518.90671 249.720021,6518.90172 249.314072,6519.28247 L249.314072,6519.28247 C248.899839,6519.67121 248.894661,6520.31179 249.302681,6520.70653 L257.196934,6528.32352 C257.601847,6528.71426 257.601847,6529.34685 257.196934,6529.73759 L249.365851,6537.29462 C248.960938,6537.68437 248.960938,6538.31795 249.365851,6538.70769"
+										id="arrow_right-[#336]"
+									></path>
+								</g>
 							</g>
 						</g>
-					</g>
-				</svg>
+					</svg>
+				</div>
 			</div>
 		</div>
 	)
