5d3ddaeb3ca44e4215eefc4a56a2aae2cfc2006f
Shiham <s36651786@gmail.com>
2024-09-24 23:03:11 +0600

    email copied set

--- src/components/Middle/Form/FormBlock.jsx
+++ src/components/Middle/Form/FormBlock.jsx
@@ -2,19 +2,37 @@ import {useState} from "react"
 
 const FormBlock = () => {
 	const [formData, setFormData] = useState({})
+	const [isCopied, setIsCopied] = useState(false)
 
-	const handleSubmit = (event) => {
-		event.preventDefault()
-	}
+	const handleSubmit = (event) => event.preventDefault()
 
 	const handleChange = (event) => {
 		const {name, value} = event.target
 		setFormData({...formData, [name]: value})
 	}
 
+	const handleEmailCopy = () => {
+		if (setIsCopied === true) return
+
+		navigator.clipboard.writeText("shihamibneyousuf@gmail.com").then(() => {
+			setIsCopied(true), setTimeout(() => setIsCopied(false), 5000)
+		})
+	}
+
 	return (
 		<form onSubmit={handleSubmit}>
-			<h6>shihamibneyousuf@gmail.com</h6>
+			<div className="relative inline-block">
+				<h6
+					className={`cursor-pointer text-2xl text-secondary transition-all duration-300 py-12 ${
+						isCopied === true && "pointer-events-none"
+					}`}
+					onClick={handleEmailCopy}
+				>
+					{isCopied ? "Email Copied" : "shihamibneyousuf@gmail.com"}
+				</h6>
+
+				<span className="absolute top-0 right-0 w-[100px] bg-secondary">Email Copied</span>
+			</div>
 			<div>
 				<label htmlFor="name">Name: </label>
 				<input type="text" name="name" value={formData.name} onChange={handleChange} />
