3cdf749ce219ca52c3154f964651b5f30d149106
Shiham <s36651786@gmail.com>
2024-09-25 23:32:21 +0600

    git setup

--- src/components/Middle/Form/FormBlock.jsx
+++ src/components/Middle/Form/FormBlock.jsx
@@ -1,33 +1,42 @@
-import {useState} from "react"
+import { useState } from "react";
 
 // redux function
 
 const FormBlock = () => {
-	// state
-	const [formData, setFormData] = useState({})
+  // state
+  const [formData, setFormData] = useState({});
 
-	const handleSubmit = (event) => event.preventDefault()
+  const handleSubmit = (event) => event.preventDefault();
 
-	const handleChange = (event) => {
-		const {name, value} = event.target
-		setFormData({...formData, [name]: value})
-	}
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<h6 className="text-2xl text-secondary p-4 border-[1px] rounded-[24px] border-primaryBorder hover:bg-secondary hover:text-primary inline-block transition-all duration-300">
-				<span className="cursor-pointer italic">
-					<a href="mailto:shihamibneyousuf@gmail.com">shihamibneyousuf@gmail.com</a>
-				</span>
-			</h6>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h6 className="text-2xl text-secondary p-4 border-[1px] rounded-[24px] border-primaryBorder hover:bg-secondary hover:text-primary inline-block transition-all duration-300">
+        <span className="cursor-pointer italic">
+          <a href="mailto:shihamibneyousuf@gmail.com">
+            shihamibneyousuf@gmail.com
+          </a>
+        </span>
+      </h6>
 
-			<div>
-				<label htmlFor="name">Name: </label>
-				<input type="text" name="name" value={formData.name} onChange={handleChange} />
-				<button type="submit">Submit Data</button>
-			</div>
-		</form>
-	)
-}
+      <div>
+        <label htmlFor="name" className="text-primaryFont font-Poppins">
+          Name:
+        </label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit Data</button>
+      </div>
+    </form>
+  );
+};
 
-export default FormBlock
+export default FormBlock;
