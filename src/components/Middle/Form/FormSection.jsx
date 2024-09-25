f11487e36616a60d36f65f55a872819af4abdba7
Shiham <s36651786@gmail.com>
2024-09-25 23:38:20 +0600

    my tailwind not working

--- src/components/Middle/Form/FormSection.jsx
+++ src/components/Middle/Form/FormSection.jsx
@@ -1,17 +1,17 @@
-import {IoMdMail} from "react-icons/io"
-import LittleHeading from "../../../Shared/LittleHeading"
-import SubHeading from "../../../Shared/SubHeading"
-import FormBlock from "./FormBlock"
+import { IoMdMail } from "react-icons/io";
+import LittleHeading from "../../../Shared/LittleHeading";
+import SubHeading from "../../../Shared/SubHeading";
+import FormBlock from "./FormBlock";
 
 const FormSection = () => {
-	return (
-		<div className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem] rounded-[20px] relative">
-			<LittleHeading headingText="Contact" Icon={IoMdMail} />
-			<SubHeading mainText="You Wanna" spanText="Contact Me?" />
+  return (
+    <div className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:px-[10%] mt-[15%] my-[10rem] rounded-[20px] relative">
+      <LittleHeading headingText="Contact" Icon={IoMdMail} />
+      <SubHeading mainText="You Wanna" spanText="Contact Me?" />
 
-			<FormBlock />
-		</div>
-	)
-}
+      <FormBlock />
+    </div>
+  );
+};
 
-export default FormSection
+export default FormSection;
