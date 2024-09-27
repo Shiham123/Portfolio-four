b13bd4464ce15122c38eff5cd0546daa487218f1
Shiham <s36651786@gmail.com>
2024-09-27 09:39:41 +0600

    input handle from other component

--- src/components/Middle/Form/FormBlock.jsx
+++ src/components/Middle/Form/FormBlock.jsx
@@ -1,49 +1,25 @@
-import { useState } from "react";
-
-// redux function
+import InputField from "./InputField"
 
 const FormBlock = () => {
-  // state
-  const [formData, setFormData] = useState({});
+	const handleSubmit = (event) => event.preventDefault()
 
-  const handleSubmit = (event) => event.preventDefault();
+	return (
+		<form onSubmit={handleSubmit}>
+			<h6 className="text-2xl text-secondary p-4 border-[1px] rounded-[24px] border-primaryBorder hover:bg-secondary hover:text-primary inline-block transition-all duration-300">
+				<span className="cursor-pointer italic">
+					<a href="mailto:shihamibneyousuf@gmail.com">shihamibneyousuf@gmail.com</a>
+				</span>
+			</h6>
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
+			<div className="pt-10">
+				<div className="flex justify-start items-start gap-4">
+					<InputField labelText="Full Name" placeholderText="Your Full Name" nameType="name" />
+					<InputField labelText="Email" placeholderText="Your Email Address" nameType="email" />
+				</div>
+				<button type="submit">Submit Data</button>
+			</div>
+		</form>
+	)
+}
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h6 className="text-2xl text-secondary p-4 border-[1px] rounded-[24px] border-primaryBorder hover:bg-secondary hover:text-primary inline-block transition-all duration-300">
-        <span className="cursor-pointer italic">
-          <a href="mailto:shihamibneyousuf@gmail.com">
-            shihamibneyousuf@gmail.com
-          </a>
-        </span>
-      </h6>
-
-      <div className="pt-10">
-        <div className="flex flex-col justify-center items-start gap-4">
-          <label
-            htmlFor="name"
-            className="text-primaryFont text-[18px] font-OpenSans uppercase"
-          >
-            Full Name:
-          </label>
-          <input
-            placeholder="your name"
-            className="bg-transparent capitalize"
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Submit Data</button>
-      </div>
-    </form>
-  );
-};
-
-export default FormBlock;
+export default FormBlock
---
+++ src/components/Middle/Form/InputField.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types"
+
+const InputField = (props) => {
+	const {labelText, placeholderText, nameType} = props
+	return (
+		<div className="flex flex-col w-1/2 gap-4">
+			<label htmlFor="name" className="text-primaryFont text-[18px] font-OpenSans uppercase">
+				{labelText}:
+			</label>
+			<input
+				placeholder={placeholderText}
+				className="bg-transparent text-[18px] hover:text-secondary focus:caret-white focus:outline-none"
+				type="text"
+				name={nameType}
+			/>
+		</div>
+	)
+}
+
+export default InputField
