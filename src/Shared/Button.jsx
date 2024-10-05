3e5f69ee06ed3d115ad48b3e77c69e4099678875
Shiham <s36651786@gmail.com>
2024-10-05 16:21:48 +0600

    heading setup

--- src/Shared/Button.jsx
+++ src/Shared/Button.jsx
@@ -2,21 +2,11 @@ import PropTypes from "prop-types"
 import {CiMail} from "react-icons/ci"
 import {useDispatch, useSelector} from "react-redux"
 import {hoverChange} from "../app/theme"
-import {useEffect, useState} from "react"
 
 const Button = (props) => {
 	const {text, btnType} = props
 	const {isHover} = useSelector((state) => state.theme)
 	const dispatch = useDispatch()
-	const [iconSize, setIconSize] = useState(30)
-
-	useEffect(() => {
-		if (window.innerWidth < 1525) {
-			setIconSize(20)
-		} else if (window.innerWidth > 1280) {
-			setIconSize(30)
-		}
-	}, [])
 
 	return (
 		<button
@@ -25,7 +15,7 @@ const Button = (props) => {
 			onMouseLeave={() => dispatch(hoverChange(false))}
 			className="border-[1px] border-[#27e68a] 2xl:mb-12 xl:mb-6 2xl:py-4 xl:py-3 lg:mb-8 lg:py-3 md:py-3 md:my-4 sm:py-4 sm:my-6 xs:my-6 xs:py-4 xxs:py-4 xxs:my-4 mx-10 rounded-custom bg-[#27e68a] group hover:bg-primary flex justify-center items-center gap-4 hover:transition-all hover:duration-300 hover:ease-in-out"
 		>
-			<CiMail size={iconSize} color={`${isHover ? " #27e68a" : "#1f1f1f"}`} />
+			<CiMail size={30} color={`${isHover ? " #27e68a" : "#1f1f1f"}`} />
 			<span className="font-Inter font-medium 2xl:text-xl xl:text-[15px] lg:text-lg md:text-lg sm:text-lg xs:text-lg uppercase leading-3 tracking-wide group-hover:text-[#27e68a]">
 				{text}
 			</span>
