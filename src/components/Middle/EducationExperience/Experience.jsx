17cdb765de2ada1adf568e90fef54db496027c8b
Shiham <s36651786@gmail.com>
2024-10-14 22:44:52 +0600

    todo is added

--- src/components/Middle/EducationExperience/Experience.jsx
+++ src/components/Middle/EducationExperience/Experience.jsx
@@ -5,6 +5,7 @@ import StepperComponent from "./StepperComponent"
 import SubHeading from "../../../Shared/SubHeading"
 
 const Experience = () => {
+	// TODO: here are some fixing needed in this experience section
 	return (
 		<section className="2xl:px-[7%] md:px-[10%] sm:px-[10%] lg:px-[10%] xl:px-[10%] xxs:mx-[20px] xxs:mb-[40px] overflow-hidden">
 			<LittleHeading Icon={FaBriefcase} headingText="Resume" />
